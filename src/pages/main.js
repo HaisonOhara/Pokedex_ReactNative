762be627fca063017f649956132f635812ca23cc
DevShark <haison.ohara@outlook.com>
2020-02-18 14:44:01 -0300

    V1-StyleSheet externalizado

--- src/pages/main.js
+++ src/pages/main.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import api from '../services/api';
 import { View, Image, Text, FlatList, TouchableOpacity, StyleSheet } from "react-native";
-
+import styles from '../styles/mainStylesheet'
 export default class Main extends Component {
     static navigationOptions = {
         title: "Pokedex"
@@ -81,56 +81,3 @@ export default class Main extends Component {
         );
     }
 }
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: "#FF6347"
-    },
-    list: {
-        padding: 20
-    },
-    pokemonContainer: {
-        backgroundColor: '#FFF',
-        borderWidth: 1,
-        borderColor: '#DDD',
-        borderRadius: 5,
-        padding: 20,
-        marginBottom: 20,
-    },
-    pokemonName: {
-        textTransform: 'capitalize',
-        fontSize: 18,
-        fontWeight: "bold",
-        color: "#333",
-    },
-    pokemonDescription: {
-        fontSize: 16,
-        color: "#999",
-        marginTop: 5,
-        lineHeight: 24
-    },
-    pokemonButton: {
-        height: 42,
-        borderRadius: 5,
-        borderWidth: 2,
-        borderColor: "#B22222",
-        backgroundColor: "#B22222",
-        justifyContent: 'center',
-        alignItems: 'center',
-        marginTop: 10
-    },
-    pokemonButtonText: {
-        fontSize: 16,
-        color: "#FFFAF0",
-        fontWeight: "bold"
-    },
-    Image: {
-        flexDirection: 'column',
-        justifyContent: 'center',
-        alignItems: 'center',
-        height: 150,
-        width: 150,
-
-    }
-
-});
